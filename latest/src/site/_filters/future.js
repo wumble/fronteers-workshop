77d2ca3e7a62f71f51382e5a9b51a762292c1b83
Phil Hawksworth <phil@netify.com>
2019-01-25 08:19:30 +0100

    remove latest

--- latest/src/site/_filters/future.js
+++
@@ -1,8 +0,0 @@
-/*
-  Return true if a date is in the future, or false if it is in the past.
-*/
-module.exports = function(date) {
-  var when = new Date(date);
-  var now = new Date();
-  return now - when < 0 ? true : false;
-}
